a6fe17787793eb836b711625cb9f3715c1a31456
VR <vladrymarchuk@gmail.com>
2024-07-06 20:48:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+import os
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    current_dir = os.path.dirname(__file__)
+    config_path = os.path.join(current_dir, "config.json")
+
+    with open(config_path, "r") as file:
+        config = json.load(file)
+
+    for customer in config["customers"]:
+        print(Customer(
+            customer,
+            Shop(config["shops"]),
+            config["FUEL_PRICE"]).return_()
+        )
+
+
+if __name__ == "__main__":
+    shop_trip()
